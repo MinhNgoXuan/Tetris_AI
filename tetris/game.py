6434222032671eb24b58c4997bd5108b7bb46fb5
MinhNgoXuan <minh.ngoxuan22@gmail.com>
2023-07-16 21:28:06 +0700

    commit

---
+++ tetris/game.py
@@ -0,0 +1,346 @@
+from random import randrange as rand
+import pygame, sys
+from ai import Ai
+from tetris import TetrisAppAI
+
+weight = [0.39357083734159515, -1.8961941343266449, -5.107694873375318, -3.6314963941589093, -2.9262681134021786, -2.146136640641482, -7.204192964669836, -3.476853402227247, -6.813002842291903, 4.152001386170861, -21.131715861293525, -10.181622180279133, -5.351108175564556, -2.6888972099986956, -2.684925769670947, -4.504495386829769, -7.4527302422826, -6.3489634714511505, -4.701455626343827, -10.502314845278828, 0.6969259450910086, -4.483319180395864, -2.471375907554622, -6.245643268054767, -1.899364785170105, -5.3416512085013395, -4.072687054171711, -5.936652569831475, -2.3140398163110643, -4.842883337741306, 17.677262456993276, -4.42668539845469, -6.8954976464473585, 4.481308299774875]
+# The configuration
+cell_size = 30
+cols = 10
+rows = 22
+maxfps = 30
+
+colors = [
+    (0, 0, 0),
+    (220, 0, 20),
+    (60, 165, 50),
+    (100, 100, 255),
+    (255, 90, 0),
+    (255, 200, 40),
+    (160, 50, 210),
+    (70, 230, 210),
+    (35, 35, 35)  # Helper color for background grid
+]
+
+# Define the shapes of the single parts
+tetris_shapes = [
+    [[1, 1, 1],
+     [0, 1, 0]],
+
+    [[0, 2, 2],
+     [2, 2, 0]],
+
+    [[3, 3, 0],
+     [0, 3, 3]],
+
+    [[4, 0, 0],
+     [4, 4, 4]],
+
+    [[0, 0, 5],
+     [5, 5, 5]],
+
+    [[6, 6, 6, 6]],
+
+    [[7, 7],
+     [7, 7]]
+]
+
+
+def rotate_clockwise(shape):
+    return [[shape[y][x]
+             for y in range(len(shape))]
+            for x in range(len(shape[0]) - 1, -1, -1)]
+
+
+def check_collision(board, shape, offset):
+    off_x, off_y = offset
+    for cy, row in enumerate(shape):
+        for cx, cell in enumerate(row):
+            try:
+                if cell and board[cy + off_y][cx + off_x]:
+                    return True
+            except IndexError:
+                return True
+    return False
+
+
+def remove_row(board, row):
+    del board[row]
+    return [[0 for i in range(cols)]] + board
+
+
+def join_matrixes(mat1, mat2, mat2_off):
+    off_x, off_y = mat2_off
+    for cy, row in enumerate(mat2):
+        for cx, val in enumerate(row):
+            mat1[cy + off_y - 1][cx + off_x] += val
+    return mat1
+
+
+def new_board():
+    board = [[0 for x in range(cols)]
+             for y in range(rows)]
+    board += [[1 for x in range(cols)]]
+    return board
+
+
+class TetrisApp(object):
+    def __init__(self):
+        pygame.init()
+        pygame.key.set_repeat(250, 25)
+        self.width = cell_size * (cols + 6)
+        self.height = cell_size * rows
+        self.rlim = cell_size * cols
+        self.bground_grid = [[8 if x % 2 == y % 2 else 0 for x in range(cols)] for y in range(rows)]
+
+        self.default_font = pygame.font.Font(
+            pygame.font.get_default_font(), 12)
+
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        pygame.event.set_blocked(pygame.MOUSEMOTION)  # We do not need
+        # mouse movement
+        # events, so we
+        # block them.
+        self.next_stone = tetris_shapes[rand(len(tetris_shapes))]
+        self.init_game()
+
+        self.nbPiece = 0
+        self.ai_playing = False
+
+    def new_stone(self):
+        self.stone = self.next_stone[:]
+        self.next_stone = tetris_shapes[rand(len(tetris_shapes))]
+        self.stone_x = int(cols / 2 - len(self.stone[0]) / 2)
+        self.stone_y = 0
+
+        if check_collision(self.board,
+                           self.stone,
+                           (self.stone_x, self.stone_y)):
+            self.gameover = True
+
+    def init_game(self):
+        self.board = new_board()
+        self.new_stone()
+        self.level = 1
+        self.score = 0
+        self.lines = 0
+        pygame.time.set_timer(pygame.USEREVENT + 1, 1000)
+
+    def disp_msg(self, msg, topleft):
+        x, y = topleft
+        for line in msg.splitlines():
+            self.screen.blit(
+                self.default_font.render(
+                    line,
+                    False,
+                    (255, 255, 255),
+                    (0, 0, 0)),
+                (x, y))
+            y += 14
+
+    def center_msg(self, msg):
+        for i, line in enumerate(msg.splitlines()):
+            msg_image = self.default_font.render(line, False,
+                                                 (255, 255, 255), (0, 0, 0))
+
+            msgim_center_x, msgim_center_y = msg_image.get_size()
+            msgim_center_x //= 2
+            msgim_center_y //= 2
+
+            self.screen.blit(msg_image, (
+                self.width // 2 - msgim_center_x,
+                self.height // 2 - msgim_center_y + i * 22))
+
+    def draw_matrix(self, matrix, offset):
+        off_x, off_y = offset
+        for y, row in enumerate(matrix):
+            for x, val in enumerate(row):
+                if val:
+                    pygame.draw.rect(
+                        self.screen,
+                        colors[val],
+                        pygame.Rect(
+                            (off_x + x) *
+                            cell_size,
+                            (off_y + y) *
+                            cell_size,
+                            cell_size,
+                            cell_size), 0)
+
+    def add_cl_lines(self, n):
+        linescores = [0, 40, 100, 300, 1200]
+        self.lines += n
+        self.score += linescores[n] * self.level
+        if self.lines >= self.level * 6:
+            self.level += 1
+            newdelay = 1000 - 50 * (self.level - 1)
+            newdelay = 100 if newdelay < 100 else newdelay
+            pygame.time.set_timer(pygame.USEREVENT + 1, newdelay)
+
+    def move(self, delta_x):
+        if not self.gameover and not self.paused:
+            new_x = self.stone_x + delta_x
+            if new_x < 0:
+                new_x = 0
+            if new_x > cols - len(self.stone[0]):
+                new_x = cols - len(self.stone[0])
+            if not check_collision(self.board,
+                                   self.stone,
+                                   (new_x, self.stone_y)):
+                self.stone_x = new_x
+
+    def quit(self):
+        self.center_msg("Exiting...")
+        pygame.display.update()
+        sys.exit()
+
+    def drop(self, manual):
+        if not self.gameover and not self.paused:
+            self.score += 1 if manual else 0
+            self.stone_y += 1
+            if check_collision(self.board,
+                               self.stone,
+                               (self.stone_x, self.stone_y)):
+                self.board = join_matrixes(
+                    self.board,
+                    self.stone,
+                    (self.stone_x, self.stone_y))
+                self.new_stone()
+                cleared_rows = 0
+                while True:
+                    for i, row in enumerate(self.board[:-1]):
+                        if 0 not in row:
+                            self.board = remove_row(
+                                self.board, i)
+                            cleared_rows += 1
+                            break
+                    else:
+                        break
+                self.add_cl_lines(cleared_rows)
+                return True
+        return False
+
+    def insta_drop(self):
+        if not self.gameover and not self.paused:
+            while (not self.drop(True)):
+                pass
+
+    def rotate_stone(self):
+        if not self.gameover and not self.paused:
+            new_stone = rotate_clockwise(self.stone)
+            if not check_collision(self.board,
+                                   new_stone,
+                                   (self.stone_x, self.stone_y)):
+                self.stone = new_stone
+
+    def toggle_pause(self):
+        self.paused = not self.paused
+
+    def toggle_ai(self):
+        self.ai_playing = not self.ai_playing
+
+    def start_game(self):
+        if self.gameover:
+            self.init_game()
+            self.gameover = False
+
+    def button(self, msg, x,y,w,h,ic,ac, action = None):
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+        print(click)
+        #topleft = (self.rlim + cell_size , cell_size * 5 +42 , 60, 150)
+        if x + w > mouse[0] > x and y + h > mouse[1] > y:
+            pygame.draw.rect(self.screen, ac, (x, y, w, h))
+
+            if click[0] == 1 and action != None:
+                action()
+        else:
+            pygame.draw.rect(self.screen, ic, (x, y, w, h))
+
+        smallText = pygame.font.SysFont("comicsansms", 20)
+        textSurf = smallText.render(msg, True, (255, 255, 255))
+        textRect = textSurf.get_rect()
+        #textSurf, textRect = text_objects(msg, smallText)
+        textRect.center = ((x + (w / 2)), (y + (h / 2)))
+        self.screen.blit(textSurf, textRect)
+
+    def executes_moves(self, moves):
+        key_actions = {
+            'ESCAPE':    self.quit,
+            'LEFT':        lambda:self.move(-1),
+            'RIGHT':    lambda:self.move(+1),
+            'DOWN':        lambda:self.drop(True),
+            'UP':        self.rotate_stone,
+            'p':        self.toggle_pause,
+            'RETURN':    self.start_game,
+            'SPACE':    self.insta_drop,
+            'r':  self.toggle_ai
+        }
+        for action in moves:
+            key_actions[action]()
+
+    def run_AI(self, weights):
+        Ai.choose(self.board, self.stone, self.next_stone, self.stone_x, weights, self)
+
+    def run(self):
+        key_actions = {
+            'ESCAPE': self.quit,
+            'LEFT': lambda: self.move(-1),
+            'RIGHT': lambda: self.move(+1),
+            'DOWN': lambda: self.drop(True),
+            'UP': self.rotate_stone,
+            'p': self.toggle_pause,
+            'RETURN': self.start_game,
+            'SPACE': self.insta_drop,
+            'r': self.toggle_ai,
+        }
+        self.gameover = False
+        self.paused = False
+
+        dont_burn_my_cpu = pygame.time.Clock()
+        while 1:
+            self.screen.fill((0, 0, 0))
+            if self.gameover:
+                self.center_msg("""Game Over! \n Press Enter to continue""")
+            else:
+                if self.paused:
+                    self.center_msg("Paused")
+                else:
+                    pygame.draw.line(self.screen,
+                                     (255, 255, 255),
+                                     (self.rlim + 1, 0),
+                                     (self.rlim + 1, self.height - 1))
+                    self.disp_msg("Next:", (
+                        self.rlim + cell_size,
+                        2))
+                    self.disp_msg("Score: %d\n\nLevel: %d\n\nLines: %d" % (self.score, self.level, self.lines),
+                                  (self.rlim + cell_size, cell_size * 5))
+                    self.button("Run AI", self.rlim + cell_size , cell_size * 5 + 100 , 100, 50, (0, 200, 0), (0,255,0))
+                    self.draw_matrix(self.bground_grid, (0, 0))
+                    self.draw_matrix(self.board, (0, 0))
+                    self.draw_matrix(self.stone,
+                                     (self.stone_x, self.stone_y))
+                    self.draw_matrix(self.next_stone,
+                                     (cols + 1, 2))
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.USEREVENT + 1:
+                    if self.ai_playing:
+                        self.run_AI(weight)  # AI plays
+                        self.insta_drop()
+                    else:
+                        self.drop(False)
+                elif event.type == pygame.QUIT:
+                    self.quit()
+                elif event.type == pygame.KEYDOWN:
+                    for key in key_actions:
+                        if event.key == eval("pygame.K_" + key):
+                            key_actions[key]()
+
+            dont_burn_my_cpu.tick(maxfps)
+
+
+if __name__ == '__main__':
+    App = TetrisApp()
+    App.run()
